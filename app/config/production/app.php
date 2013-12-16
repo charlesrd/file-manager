ece9c01dae4936b7c61bf815a021a19a40b9e055
Charles Dyke <charlesrdyke@gmail.com>
2013-12-16 14:36:20 -0600

    Setting production soft cutoff hours to correct times.

--- app/config/production/app.php
+++ app/config/production/app.php
@@ -6,8 +6,8 @@ return array(
 	'site_title' => 'File Management System',
 
 	// Application configuration options set here
-	'file_upload_soft_cutoff_hour' => 20, // 3pm CST for soft cutoff time (24H format)
-	'file_upload_hard_cutoff_hour' => 21, // 4pm CST for hard cutoff time (24H format)
+	'file_upload_soft_cutoff_hour' => 15, // 3pm CST for soft cutoff time (24H format)
+	'file_upload_hard_cutoff_hour' => 16, // 4pm CST for hard cutoff time (24H format)
 	'end_of_day_hour' => 24, // at 12am CST, no more cutoff
 	'file_upload_rush_processing_fee' => 3, // $3.00 per tooth processing fee
 	'soft_cutoff_reset_hour' => 20, // 8pm CST in 24h format
